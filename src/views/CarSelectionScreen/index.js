0554901be234ec4e29ce889709ab917edfab781b
HammadMaqsood786 <116306657+HammadMaqsood786@users.noreply.github.com>
2023-01-06 01:56:58 +0500

    Add files via upload

--- src/views/CarSelectionScreen/index.js
+++ src/views/CarSelectionScreen/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-function CarSelection () {
-    return(
+function CarSelection() {
+
+    // const carIcon = "https://cdn-icons-png.flaticon.com/512/3202/3202926.png";
+
+    return (
         <View style={styles.container} >
             <View style={styles.vehicleBox} >
-                
+                <View style={styles.carsBox} >
+                    <View style={styles.bigCarBox} onPress={() => styles = { borderWidth: 2, borderColor: '#1a8cff', }} >
+                        <Image style={styles.bigCarIcon} source={{ uri: "https://cdn-icons-png.flaticon.com/512/9175/9175833.png" }} />
+                    </View>
+
+                    <View style={styles.smallCarBox} >
+                        <Image style={styles.smallCarIcon} source={{ uri: "https://cdn-icons-png.flaticon.com/512/3202/3202926.png" }} />
+                    </View>
+
+                </View>
+
+                <View style={styles.carsBox2} >
+                    <View style={styles.bikeBox}  >
+                        <Image style={styles.bikeIcon} source={{ uri: "https://cdn-icons-png.flaticon.com/512/1168/1168041.png" }} />
+                    </View>
+
+                    <View style={styles.autoBox} >
+                        <Image style={styles.autoIcon} source={{ uri: "https://cdn-icons-png.flaticon.com/512/3790/3790389.png" }} />
+                    </View>
+
+                </View>
+
+                <View style={styles.pickupBtnContainer} >
+                    <TouchableOpacity style={styles.pickupBtn}  >
+                        <Text style={{ fontSize: 19, }} >Vehicle Selected</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
@@ -15,7 +44,90 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#252526',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    vehicleBox: {
+        width: '90%',
+        height: '50%',
+        backgroundColor: '#2E2E2E',
+        borderRadius: 10,
+
+    },
+    carsBox: {
+        width: '100%',
+        height: '50%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 10,
+
+    },
+    bigCarBox: {
+        width: '45%',
+        height: '80%',
+        borderWidth: 2,
+        borderColor: '#1a8cff',
+        borderRadius: 8,
+        // // backgroundColor: 'white',
+    },
+    bigCarIcon: {
+        width: '100%',
+        height: '80%',
+    },
+    smallCarBox: {
+        width: '45%',
+        height: '80%',
+        backgroundColor: 'white',
+    },
+    smallCarIcon: {
+        width: '100%',
+        height: '100%',
+    },
+    carsBox2: {
+        width: '100%',
+        height: '50%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    bikeBox: {
+        width: '45%',
+        height: '80%',
+        backgroundColor: 'white',
+    },
+    bikeIcon: {
+        width: '80%',
+        height: '85%',
+        marginLeft: 10,
+    },
+    autoBox: {
+        width: '45%',
+        height: '80%',
+        backgroundColor: 'white',
+    },
+    autoIcon: {
+        width: '85%',
+        height: '80%',
+        marginTop: 5,
+        marginLeft: 9,
+    },
+    pickupBtnContainer: {
+        width: '100%',
+        height: '20%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    pickupBtn: {
+
+        width: '95%',
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: '#00e600',
     }
+
 })
 
 export default CarSelection;
--- src/views/DestinationScreen/index.js
+++ src/views/DestinationScreen/index.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
 function DestinationScreen({ route, navigation }) {
 
-    const {currentLocation} = route.params;
-    console.log("Pickup Location =======>",currentLocation);
-    
+    const { currentLocation } = route.params;
+
+    // console.log("current location =======>", currentLocation);
+
+    const { longitude, latitude } = currentLocation;
+
+    // console.log("longitude =======>", longitude);
+    // console.log("latitiude =======>", latitude);
+
+
     const [location, setLocation] = useState({
         latitude: 24.8952922,
         longitude: 67.0823298,
@@ -15,15 +23,81 @@ function DestinationScreen({ route, navigation }) {
         longitudeDelta: 0.0012,
     });
 
- 
-    
-    
-    useEffect(() => {
-        (async () => {
+    const [query, setQuery] = useState("");
+    const [destination, setDestination] = useState([])
 
-          
-        })();
-    }, []);
+
+    const searchPlaces = (text) => {
+        setQuery(text);
+        console.log("query =====>", query);
+    }
+
+    useEffect(() => {
+        // const options = {
+        //     method: 'GET',
+        //     headers: {
+        //       accept: 'application/json',
+        //       Authorization: 'fsq3YmyU2CvERiMkIOaa+4hZR3EMy0w1HinrEWAvZC/b9JQ='
+        //     }
+        //   };
+
+        //   const endPoint = "https://api.foursquare.com/v3/places/search";
+
+        //   const parameters = {
+        //     clientId: "HW2V033ZIP5QGBSGOUXSGUU15EC2XAD3OM41Z2DV31LPJ2RV",
+        //     clientSecret: "MIFQRM0VXZX5IJCX20S5PXDCOPVGJR4KD2HODTJWU55RAGNZ",
+        //     query: "national",
+        //     v: "20230101"
+
+        // }
+
+        // axios.get(endPoint + new URLSearchParams(parameters))
+        // .then(response => {
+        //   console.log("data", response)
+        // })
+        // .catch(error => {
+        //   console.log("ERROR!! " + error)
+        // })
+
+        //============ Foursquare api code ===============//
+
+        const radius = '300';
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'fsq3YmyU2CvERiMkIOaa+4hZR3EMy0w1HinrEWAvZC/b9JQ='
+            }
+        };
+
+        fetch(`https://api.foursquare.com/v3/places/search?query=${query}&ll=${latitude}%2C${longitude}&radius=100000`, options)
+
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setDestination(response.results[0].geocodes.main)
+
+            })
+            // .then(response => )
+            .catch(err => console.error(err));
+
+        console.log("geocodes", destination);
+
+
+        // const latitiude = destination.latitude;
+        // const longitude = destination.longitude;
+    
+        // // console.log("latitude", destLatitude);
+        // // console.log("longitude", destLongitude);
+    
+        // setLocation({...location, latitude, longitude});
+    
+        // console.log('location baad wali', location);
+
+    }, [query]);
+
+
 
     return (
         <View style={styles.container} >
@@ -35,15 +109,17 @@ function DestinationScreen({ route, navigation }) {
                     description={'Default Location'}
                 />
             </MapView>
-            
-            <TextInput style={styles.searchBar} cursorColor="black"  >
-                
+
+            <TextInput style={styles.searchBar} cursorColor="black" placeholder='Search' onChangeText={(text) => searchPlaces(text)}>
+
             </TextInput>
+
             <View style={styles.pickupBtnContainer} >
                 <TouchableOpacity style={styles.pickupBtn} onPress={() => navigation.navigate('Cars')}  >
                     <Text style={{ fontSize: 19, }} >Confirm Destination</Text>
                 </TouchableOpacity>
             </View>
+
         </View>
     );
 }
@@ -60,14 +136,16 @@ const styles = StyleSheet.create({
     searchBar: {
         flex: 1,
         width: '90%',
-        height: '8%', 
+        height: '8%',
         backgroundColor: 'white',
         position: 'absolute',
         marginTop: 10,
         marginLeft: '5%',
         marginRight: '5%',
+        borderWidth: 1,
         borderColor: 'black',
         borderRadius: 30,
+
 
     },
     pickupBtnContainer: {
